db445ad61a2e50bb02bfc0b8b9f125231c4f3ad0
YujinHa <hamonica98@naver.com>
2019-02-18 14:35:45 +0900

    add slate

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import nuke
 
 tb = nuke.toolbar("Nodes")
-m = tb.addMenu("Lazypic")
+m = tb.addMenu("Mingco", icon="mingco.png")
 m.addMenu("Draw")
 m.addCommand("Draw/timecode_burnin","nuke.createNode('timecode_burnin')")
+m.addCommand("Draw/slate","nuke.createNode('slate')")
