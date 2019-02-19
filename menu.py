13b23bec32b0a723cc42c69e029a5a6f8849b76b
YujinHa <hamonica98@naver.com>
2019-02-19 09:54:03 +0900

    add Format and Menu

--- menu.py
+++ menu.py
@@ -1,7 +1,12 @@
 import nuke
+import nukescripts
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("Mingco", icon="mingco.png")
 m.addMenu("Draw")
 m.addCommand("Draw/timecode_burnin","nuke.createNode('timecode_burnin')")
 m.addCommand("Draw/slate","nuke.createNode('slate')")
+
+mb = menubar.addMenu("Yujin")
+mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/YujinHa/nukeset/issues')")
+mb.addCommand("Slack", "nukescripts.start('https://voiceplayer.slack.com')")
